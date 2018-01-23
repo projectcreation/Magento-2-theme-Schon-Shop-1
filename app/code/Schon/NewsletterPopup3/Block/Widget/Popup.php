dbd42d2b9d9cb0133d16cd8e0c46ce8d3c6359be
Gorchakov Alexandr <gradual.development.mail@gmail.com>
2018-01-23 13:45:53 +0600

    add Module: Schon_NewsletterPopup3 desc: this module add custom newsletter popup with markup( window with input field and submit button ) from HTML template. Popup is opened across 5 seconds after DOM loaded

---
+++ app/code/Schon/NewsletterPopup3/Block/Widget/Popup.php
@@ -0,0 +1,24 @@
+<?php
+//namespace Schon\NewsletterPopup3\Block\Widget;
+//
+//
+//use Magento\Framework\View\Element\Template;
+//use Magento\Widget\Block\BlockInterface;
+//
+//class Popup extends Template implements BlockInterface
+//{
+//	protected $_template = "popup-tpl-3.phtml";
+//}
+
+
+namespace Schon\NewsletterPopup3\Block\Widget;
+
+use Magento\Framework\View\Element\Template;
+use Magento\Widget\Block\BlockInterface;
+
+class Popup extends Template implements BlockInterface
+{
+
+	protected $_template = "popup-tpl-3.phtml";
+
+}
