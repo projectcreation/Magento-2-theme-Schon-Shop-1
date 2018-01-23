0e7aa4e76ecc89fbcace1fd52268c73b203f5105
Gorchakov Alexandr <gradual.development.mail@gmail.com>
2018-01-23 13:55:53 +0600

    adminhtml: init theme MyNewNameSpace_MyAdminTheme desc: add custom logo for Storefront part and admin part ( admin sign in form )

---
+++ app/design/adminhtml/MyNewNameSpace/MyAdminTheme/registration.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alex
+ * Date: 06.01.18
+ * Time: 23:12
+ */
+
+
+	/**
+	 * Copyright © 2016 Magento. All rights reserved.
+	 * See COPYING.txt for license details.
+	 */
+\Magento\Framework\Component\ComponentRegistrar::register(
+		\Magento\Framework\Component\ComponentRegistrar::THEME,
+		'adminhtml/MyNewNameSpace/MyAdminTheme', // Example: 'adminhtml/Magento/backend'
+		__DIR__
+	);
+
+
